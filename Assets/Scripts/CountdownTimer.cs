4bfc2feb31c8b5246cd13eef4850077e4172b76e
antonysme1 <asmelian@rams.colostate.edu>
2020-03-24 15:23:43 -0600

    Added oculus integration, and environment with a timer

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    float currentTime = 0;
+    float startingTime = 10f;
+    //Text in the canvas using UI Text
+    //Seralized Field is used so I can see the variable in unity
+   [SerializeField] Text countdownText;
+
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    void Update()
+    {
+        if (currentTime > 0)
+        {
+            //makes timer decrease by one second
+            currentTime -= 1 * Time.deltaTime;
+            print(currentTime);
+            countdownText.text = currentTime.ToString("0");
+        }
+
+        if (currentTime >= 3.5f) {
+            countdownText.color = Color.black;
+        }
+        //Change color to red when under 4 seconds
+        if (currentTime < 3.5f) {
+            countdownText.color = Color.red;
+        }
+
+        if (currentTime <= 0)
+        {
+            //Hides the countdown timer after 1 second
+            Invoke("Hide", 1);
+        }
+
+    }
+
+    void Hide()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/DestroyStartText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DestroyStartText : MonoBehaviour
+{
+    [SerializeField] Text StartingText;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("Hide", 11);
+    }
+
+    void Hide()
+    {
+        Destroy(gameObject);
+    }
+}
